827450c3c14b04019eeb36b8ec1281be56f3a168
Shr4528 <Shr4528@gmail.com>
2023-11-12 18:00:31 -0600

    adduser

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -29,44 +29,42 @@ const ProductsPage = () => {
           </tr>
         </thead>
         <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td>
-                <div className={styles.product}>
-                  <Image
-                    src={product.img || '/noproduct.jpg'}
-                    alt=''
-                    width={40}
-                    height={40}
-                    className={styles.productImage}
-                  />
-                  {product.title}
-                </div>
-              </td>
-              <td>{product.desc}</td>
-              <td>${product.price}</td>
-              <td>{product.createdAt?.toString().slice(4, 16)}</td>
-              <td>{product.stock}</td>
-              <td>
-                <div className={styles.buttons}>
-                  <Link href={`/dashboard/products/${product.id}`}>
-                    <button className={`${styles.button} ${styles.view}`}>
-                      View
-                    </button>
-                  </Link>
-                  <form action={deleteProduct}>
-                    <input type='hidden' name='id' value={product.id} />
-                    <button className={`${styles.button} ${styles.delete}`}>
-                      Delete
-                    </button>
-                  </form>
-                </div>
-              </td>
-            </tr>
-          ))}
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src='/noproduct.jpg'
+                  alt=''
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                {/* {product.title} */}
+              </div>
+            </td>
+            <td>''</td>
+            <td>$68688</td>
+            <td>createdAt?</td>
+            <td>stock</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={`/dashboard/products/`}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <form>
+                  <input type='hidden' name='id' />
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </td>
+          </tr>
         </tbody>
       </table>
-      <Paginition count={count} />
+      <Paginition />
     </div>
   );
 };
